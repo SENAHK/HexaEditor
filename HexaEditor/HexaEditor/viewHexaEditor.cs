1ea8dac8e93d8dd8363ad5b0d5fb9ac23b2eefec
Senahk <michael-arian.rms@eduge.ch>
2017-02-07 10:28:50 +0100

    moved writeinpicturebox to the view

--- HexaEditor/HexaEditor/viewHexaEditor.cs
+++ HexaEditor/HexaEditor/viewHexaEditor.cs
@@ -34,6 +34,34 @@ namespace HexaEditor
 
 
         }
+
+        public void WriteInPictureBox(PictureBox pbx, string[,] values)
+        {
+            Bitmap DrawArea = new Bitmap(pbx.Size.Width, pbx.Size.Height);
+            Graphics g = Graphics.FromImage(DrawArea);
+
+            int valuesX = values.GetLength(1);
+            int valuesY = values.GetLength(0);
+
+            int width = pbx.Size.Width / valuesX;
+            int height = pbx.Size.Height / valuesY;
+
+            string output = "";
+
+            for (int y = 0; y < valuesY; y++)
+            {
+                for (int x = 0; x < valuesX; x++)
+                {
+
+                    output = values[y, x];
+                    Rectangle rect = new Rectangle(x * width, y * height, width, height);
+                    g.DrawString(output, new Font("Courier New", 16), Brushes.Black, rect);
+                }
+            }
+            pbx.Image = DrawArea;
+            g.Dispose();
+        }
+
         public void generateRandomHexValues(string[,] values)
         {
             Random rnd = new Random();
